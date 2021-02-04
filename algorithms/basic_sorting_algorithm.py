ec57404e5888cc28433f1c62ba163bbc982284a5
Oscar Dealtry <odealtry@gmail.com>
2021-02-04 11:55:23 +0000

    indexing approach not working, trying to implement recursion

--- algorithms/basic_sorting_algorithm.py
+++ algorithms/basic_sorting_algorithm.py
@@ -1,10 +1,18 @@
 def basic_sort(list):
-    for element in list:
-        element_index = list.index(element)
-        previous_element = list[element_index - 1]
-        print(element_index, previous_element)
-        if element < previous_element:
-            element, previous_element = previous_element, element
+    iterator = 0
+    # iterator2 = 0
+    for i in range(len(list)):
+        if iterator > 0:
+            print(list[iterator], list[iterator - 1])
+            if list[iterator] < list[iterator - 1]:
+                list[iterator], list[iterator - 1] = list[iterator - 1], list[iterator - 1]
+            iterator += 1
+    # for element in list:
+    #     element_index = list.index(element)
+    #     previous_element = list[element_index - 1]
+    #     print(element_index, previous_element)
+    #     if element < previous_element:
+    #         element, previous_element = previous_element, element
         # print(index)
         # print(element)
     return list
